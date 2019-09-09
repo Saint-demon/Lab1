b31aa09c062ce54e875f33bf04871aa90d1991b8
Saint-demon <Saint-demon@users.noreply.github.com>
2019-09-09 23:17:54 +0300

    пук 3 выполнен. тудушки добавлены

--- Program.cs
+++ Program.cs
@@ -1,36 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 
 namespace Lab1
 {
     class ProgramLab1
     {
-        static void Main()
+        private static void Main()
         {
-            Console.WriteLine("Выберите способ ввода массивас:");
+            Console.WriteLine("Выберите способ ввода массива:");
             Console.WriteLine("0:Ввод массива вручную;");
             Console.WriteLine("1:Чтение массива из файла");
-            
+            //TODO: размер массива
             int value = Convert.ToInt32(Console.ReadLine());
 
 
-            int n = 9, min, max, even = 0;        
+            int n = 9, min, max, even = 0;
 
             int[] arr = new int[10];
             int[] evenArr = new int[10];
-            
-            for (int i = 0; i < arr.Length; i++) //Ввод массива
+            if (value == 0)
             {
-                arr[i] = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < arr.Length; i++) //Ввод массива
+                {
+                    arr[i] = Convert.ToInt32(Console.ReadLine());
+                }
             }
-            
+            else if (value == 1)
+            {
+                //TODO: ввод из  файла
+            }
+
             for (int i = 0; i < arr.Length; i++) //Вывод массива
             {
                 Console.WriteLine($"arr[{i}] = {arr[i]}");
             }
-            
+
             min = arr[0];
-            for (int i = 0; i < 10; i++) //Поиск min
+            for (int i = 0; i < arr.Length; i++) //Поиск min
             {
                 if (arr[i] <= min)
                 {
@@ -38,11 +45,12 @@ namespace Lab1
                     n = i;
                 }
             }
+
             Console.WriteLine("Минимум:");
             Console.WriteLine($"arr[{n}] = {min}");
 
             max = arr[0];
-            for (int i = 0; i < 10; i++) //Поиск max
+            for (int i = 0; i < arr.Length; i++) //Поиск max
             {
                 if (arr[i] >= max)
                 {
@@ -50,18 +58,47 @@ namespace Lab1
                     n = i;
                 }
             }
+
             Console.WriteLine("Максимум");
             Console.WriteLine($"arr[{n}] = {max}");
-            //TODO: сюда 3ий пункт добавить
-            for (int i = 0; i < arr.Length; i++)
+            int sort;
+            for (int i = 0; i < arr.Length; i++)//сортировка прямая
             {
-                if (arr[i] % 2 == 0)
+                for (int j = i + 1; j < arr.Length; j++)
                 {
-                    evenArr[even] = arr[i];
-                    even++;
+                    if (arr[i] > arr[j])
+                    {
+                        sort = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = sort;
+                    }
                 }
             }
-            
+
+
+            for (int i = 0; i < arr.Length; i++)//сортировка обратная
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] < arr[j])
+                    {
+                        sort = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = sort;
+                    }
+                }
+            }
+
+            foreach (int i in arr)
+            {
+                if (i % 2 == 0)
+                {
+                    evenArr[even] = i;
+                    even++;
+                }
+
+            }
+
 
             for (int i = 0; i < arr.Length; i++)
             {
@@ -74,5 +111,6 @@ namespace Lab1
 
         }
 
+            
+        }
     }
-}
